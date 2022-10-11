469eed6affd9306ea6d7c96260d75ee71d630825
MS-Leeffice <yhkyhk92@gmail.com>
2022-10-11 14:05:04 +0900

    background 오류 수정

--- js/background.js
+++ js/background.js
@@ -1,17 +1,17 @@
-const images = [
-    "0.jpg",
-    "1.jpg",
-    "2.jpg",
-    "3.jpg",
-    "4.jpg",
-    "5.jpg",
-    "6.jpg",
-    "7.jpg",
-];
+// const images = [
+//     "0.jpg",
+//     "1.jpg",
+//     "2.jpg",
+//     "3.jpg",
+//     "4.jpg",
+//     "5.jpg",
+//     "6.jpg",
+//     "7.jpg",
+// ];
 
-const chosenImage = images[Math.floor((Math.random() * images.length))];
+// const chosenImage = images[Math.floor((Math.random() * images.length))];
 
-const bgImage = document.createElement("img");
-bgImage.src = `img/${chosenImage}`;
+// const bgImage = document.createElement("img");
+// bgImage.src = `img/${chosenImage}`;
 
-document.body.appendChild(bgImage);
+// document.body.appendChild(bgImage);
