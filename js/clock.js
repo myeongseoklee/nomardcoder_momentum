020ff82fea0d60fbf9af3bba9a879d8100947a4b
MS-Leeffice <yhkyhk92@gmail.com>
2022-10-11 14:04:00 +0900

    css 백그라운드 적용

--- js/clock.js
+++ js/clock.js
@@ -1,6 +1,6 @@
-const clock = document.querySelector("h2#clock");
+const clock = document.querySelector("#clock");
+const today = document.querySelector("#today");
 
-// clock.innerText = "slsl"
 function getClock() {
     const date = new Date();
     const hours = String(date.getHours()).padStart(2, "0");
@@ -8,5 +8,16 @@ function getClock() {
     const minutes = String(date.getMinutes()).padStart(2, "0");
     clock.innerText = `${hours}:${minutes}:${seconds}`
 }
+
+function getToday() {
+    const date = new Date();
+    const todayMonth = String(date.getMonth() + 1).padStart(2,"0");
+    const todayDate = String(date.getDate()).padStart(2,"0");
+    const week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const todayDay = week[date.getDay()];
+    today.innerText = `${todayMonth}월 ${todayDate}일 ${todayDay}`
+}
+
 getClock();
+getToday();
 setInterval(getClock, 1000);
