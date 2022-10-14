a716662b46f041f2179d873e78133762301bbbe9
MS-Leeffice <yhkyhk92@gmail.com>
2022-10-14 18:14:25 +0900

    put something

--- js/todo.js
+++ js/todo.js
@@ -6,18 +6,18 @@ const TODOS_KEY = "todos"
 
 let toDos = [];
 
-function saveToDos(){
+function saveToDos(){ // 메모리에 저장된 정보를 localstorage에 복사
     localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
-function deleteToDo(event) {
+function deleteToDo(event) { // html, 메모리에 저장된 정보 삭제
     const li = event.target.parentElement;
     li.remove();
     toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id));
-    saveToDos();
+    saveToDos(); // 자동으로 로컬스토리지 정보 삭제
 }
 
-function paintToDo(newTodoObj) {
+function paintToDo(newTodoObj) { // 메모리에 저장된 정보로 html을 그림
     const li = document.createElement("li");
     li.id = newTodoObj.id
     const span = document.createElement("span");
@@ -30,7 +30,7 @@ function paintToDo(newTodoObj) {
     toDoList.appendChild(li);
 }
 
-function handleToDoSubmit(event) {
+function handleToDoSubmit(event) { // 제출한 정보를 memory와 로컬스토리지에 저장
     event.preventDefault();
     const newTodo = toDoInput.value
     toDoInput.value = "";
@@ -47,7 +47,7 @@ toDoForm.addEventListener("submit", handleToDoSubmit);
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
 
-if(savedToDos !== null ){
+if(savedToDos !== null ){ // 페이지 리로드 되어도 리스트 유지
     const parsedToDos = JSON.parse(savedToDos);
     toDos = parsedToDos
     parsedToDos.forEach(paintToDo);
